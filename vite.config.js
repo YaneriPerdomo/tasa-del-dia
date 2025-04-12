08ee2749cb797807572e52309e85dfad690b4c92
Yaneri Perdomo <perdomopaolabarrios@gmail.com>
2025-04-11 20:08:43 -0400

    Agregando las mismas configuraciones

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-// Ejemplo de configuración típica para Laravel + Vite
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'; // Si usas Vue
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    laravel([
-      'resources/css/app.css',
-      'resources/js/app.js',
-    ]),
-    vue(), // Si usas Vue
-  ],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
